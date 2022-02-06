0f6ea8a552bdde413d1ec67cad5918603145c528
Aleks <chernovalex311.gmail.com>
2022-02-06 19:47:37 +0700

    обавил функцию сложения

--- lastChance2.js
+++ lastChance2.js
@@ -2,17 +2,28 @@ let plus = document.getElementById('ButtonPlus');
 let minus = document.getElementById('ButtonMinus');
 let multiplication = document.getElementById('ButtonMultiplication');
 let division = document.getElementById('ButtonDivision');
+let n1 = document.getElementById('Number1');
+let n2 = document.getElementById('Number2');
 
-function onPlusClic(){
-    console.log('onPlusClic')
+
+function onPlusClick() {
+    console.log('onPlusClick');
+    console.dir(n1);
+    console.log(n1.value);
+    console.dir(n2.value);
+    console.log(n1.value + n2.value);
+    num1 = Number(n1.value);
+    num2 = Number(n2.value);
+    alert(num1 + num2);
 }
-function onMinusClic(){
-    console.log('onMinusClic')
+function onMinusClick() {
+    console.log('onMinusClick');
 }
-function onMultiplicationClic(){
-    console.log('onMultiplicationClic')
+function onMultiplicationClick() {
+    console.log('onMultiplicationClick');
 }
-function onDivisionClic(){
-    console.log('onDivisionClic')
+function onDivisionClick() {
+    console.log('onDivisionClick')
 }
-plus.addEventListener('click', onPlusClic);
+plus.addEventListener('click', onPlusClick);
+
