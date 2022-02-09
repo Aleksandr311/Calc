8ec7a91f6079662162dbb658abda8cfeab3878fb
Aleks <chernovalex311.gmail.com>
2022-02-09 18:26:36 +0700

    еще сократил

--- lastChance2.js
+++ lastChance2.js
@@ -1,47 +1,36 @@
-let plus = document.getElementById('ButtonPlus');
-let minus = document.getElementById('ButtonMinus');
-let multiplication = document.getElementById('ButtonMultiplication');
-let division = document.getElementById('ButtonDivision');
+//let plus = document.getElementById('ButtonPlus');
+//let minus = document.getElementById('ButtonMinus');
+//let multiplication = document.getElementById('ButtonMultiplication');
+//let division = document.getElementById('ButtonDivision');
 let n1 = document.getElementById('Number1');
 let n2 = document.getElementById('Number2');
-
-/*function getNumber1(){
-    return Number(n1.value);
-}
-function getNumber2(){
-    return Number(n2.value);
-} 
-*/
-function makeOperation(operayionCode){
+let arrayOfButtons = document.getElementsByClassName('operationButton')
+function makeOperation(operayionCode) {
     let num1 = Number(n1.value);
     let num2 = Number(n2.value);
-    if (operayionCode === '+'){
+    if (operayionCode === '+') {
         alert(num1 + num2);
     }
-    else if (operayionCode === '-'){
+    else if (operayionCode === '-') {
         alert(num1 - num2);
     }
-    else if (operayionCode === '*'){
+    else if (operayionCode === '*') {
         alert(num1 * num2);
     }
-    else if (operayionCode === '/'){
+    else if (operayionCode === '/') {
         alert(num1 / num2);
     }
     else {
         alert('operation is unknow');
     }
-    //alert(getNumber1() + getNumber2());
 }
-
-
-function onOperationButtonClick(eventObject){
-    //debugger;
+function onOperationButtonClick(eventObject) {
     let clickedElement = eventObject.currentTarget;
     let operation = clickedElement.innerHTML;
     makeOperation(operation);
 }
+//let arrayOfButtons = [plus, minus, multiplication, division];
 
-plus.addEventListener('click', onOperationButtonClick);
-minus.addEventListener('click', onOperationButtonClick);
-multiplication.addEventListener('click', onOperationButtonClick);
-division.addEventListener('click', onOperationButtonClick);
+for (let i = 0; i < arrayOfButtons.length; i++) {
+    arrayOfButtons[i].addEventListener('click', onOperationButtonClick);
+}
